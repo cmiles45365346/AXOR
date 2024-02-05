d568dbd50e41f6dccec427017b6d2bd55e070ab4
cmiles45365346 <103551911+cmiles45365346@users.noreply.github.com>
2024-02-05 09:18:25 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+  int private_key[256];
+  int public_key[256];
+  int output = 0;
+
+  //encode & decode
+  for (int i = 0; i <= 257; i++) 
+  {
+    if (priv_key[i] == 1 && pub_key[i] == 1) 
+    {
+        output++;
+    }
+  }
+  
+  const int result = output % 2;
+  return result >= 0 ? result : result + 2;
+}
